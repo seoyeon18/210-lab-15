d32e5386572e2ed1efb53fb5112776f89faa8761
Your Name <brightsy04@gmail.com>
2026-03-01 19:43:49 -0800

    lab15-3

--- 15.cpp
+++ 15.cpp
@@ -1,6 +1,8 @@
 // COMSC-210 | Movie Assignment | Seoyeon An
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -16,7 +18,7 @@ public:
     int getYear() const { return year; }
     string getScreenWriter() const { return screenWriter;}
 
-    // setter
+    // setter   
     void setTitle(string t) { title = t;}
     void setYear(int y) { year = y;}
     void setScreenWriter(string s) { screenWriter = s;}
@@ -27,3 +29,37 @@ public:
         cout << "Screen writer: " << screenWriter <<endl << endl;
     }
 };
+
+int main() {
+
+    vector<Movie> movies;
+    ifstream fin("input.txt");
+
+    if (!fin) {
+        cout << "Input file not found.\n";
+        return 1;
+    }
+
+    string t;
+    int y;
+    string s;
+
+    while (getline(fin, t)) {
+        fin >> y;
+        fin.ignore();
+        getline(fin, s);
+
+        Movie temp;
+        temp.setTitle(t);
+        temp.setYear(y);
+        temp.setScreenWriter(s);
+
+        movies.push_back(temp);
+    }
+
+    fin.close();
+    for (const auto& m : movies) {
+        m.print();
+    }
+    return 0;
+}
